352f177c016bbd69d2fc5668cc75632f8820f6f1
tatumd <devan.tatum@gmail.com>
2014-01-31 02:00:30 -0800

    !!!!

--- seqAlign_thread.c
+++ seqAlign_thread.c
@@ -382,7 +382,7 @@ int main(int argc, char* argv[]) {
     addValNode(&n_head, 1);
     //addValTh(&th_head, thread_id);
 
-    printf("Head: %d\n", getPosNode(&head,1));
+    printf("Head: %d\n", getPosNode(&n_head,1));
     //printf("Th_Head: %d\n", getPosTh(&th_head,1));
 
     /* could not create thread */
